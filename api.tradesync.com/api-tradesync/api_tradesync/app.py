27eaa5e2a05a4cacc95a47ac19f31e5c59a36331
Bhumi1685 <jainbhumi1685@gmail.com>
2024-03-31 00:12:53 +0530

    feat:ticker detail view route

--- api.tradesync.com/api-tradesync/api_tradesync/app.py
+++ api.tradesync.com/api-tradesync/api_tradesync/app.py
@@ -16,7 +16,7 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 app = Flask(__name__)
 CORS(app)
-
+TICKER_URL = "https://api.polygon.io/v3/reference/tickers/"
 BASE_URL = "https://api.polygon.io/v2/aggs"
 NEWS_URL = "https://api.polygon.io/v2/reference/news"
 API_URL = "https://api-inference.huggingface.co/models/ProsusAI/finbert"
@@ -60,11 +60,11 @@ def get_stock_aggregates(stocksTicker, multiplier, timespan, from_date, to_date)
     response = requests.get(url)
     
     # Check if the request was successful
+     
     if response.status_code == 200:
         # Return the JSON response from Polygon API
         return jsonify(response.json()), 200
     else:
-        # Return an error message if something went wrong with the request
         return jsonify({"error": "Failed to fetch data from Polygon API", "status_code": response.status_code}), response.status_code
 
 
@@ -83,6 +83,17 @@ def get_group_aggregates(date):
     else:
         # Return an error message if something went wrong with the request
         return jsonify({"error": "Failed to fetch data from Polygon API", "status_code": response.status_code}), response.status_code
+        
+@app.route('/stock-ticker/<string:tickerName>', methods=['GET'])
+def get_stock_news(tickerName):
+    # Construct the URL for the Polygon API request
+    url = f"{BASE_URL}/{tickerName}?&limit=5apiKey={os.getenv('POLYGON_API_KEY')}"
+    
+    if response.status_code == 200:
+        # Return the JSON response from Polygon API
+        return jsonify(response.json()), 200
+    else:
+        # Return an error messaage
         
 if __name__ == '__main__':
     app.run(debug=True)
