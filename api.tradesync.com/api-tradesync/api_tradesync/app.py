0f62eec4e3df74f6067507a10cfcb9b0aa0ae400
prathik shetty <prathikkshetty15@gmail.com>
2024-03-30 17:59:46 +0530

    feat: api routes for ticker

--- api.tradesync.com/api-tradesync/api_tradesync/app.py
+++ api.tradesync.com/api-tradesync/api_tradesync/app.py
@@ -17,14 +17,58 @@ genai.configure(api_key=GOOGLE_API_KEY)
 app = Flask(__name__)
 CORS(app)
 
+BASE_URL = "https://api.polygon.io/v2/aggs"
+
 API_URL = "https://api-inference.huggingface.co/models/ProsusAI/finbert"
 headers = {"Authorization": "Bearer " +  os.getenv('API_KEY') }
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
-output = query({
-	"inputs": " IPOs in FY24 help raise over Rs 61,900 crore: Fundraising up 20%, highest in two years",
-})
 
+@app.route('/finbert-query', methods=['GET'])
+def finbert_query():
+    # Ensure there's a JSON payload
+    if not request.json or 'inputs' not in request.json:
+        return jsonify({'error': 'No input provided'}), 400
+    
+    inputs = request.json['inputs']
+    payload = {"inputs": inputs}
+    output = query(payload)
+    return jsonify(output)
+
+@app.route('/stock-aggregates/<string:stocksTicker>/<int:multiplier>/<string:timespan>/<string:from_date>/<string:to_date>', methods=['GET'])
+def get_stock_aggregates(stocksTicker, multiplier, timespan, from_date, to_date):
+    # Construct the URL for the Polygon API request
+    url = f"{BASE_URL}/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={os.getenv('POLYGON_API_KEY')}"
+    
+    # Make the request to the Polygon API
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Return the JSON response from Polygon API
+        return jsonify(response.json()), 200
+    else:
+        # Return an error message if something went wrong with the request
+        return jsonify({"error": "Failed to fetch data from Polygon API", "status_code": response.status_code}), response.status_code
+
+
+# @app.route('/group-aggregates/<string:date>', methods=['GET'])
+# def get_group_aggregates(stocksTicker, multiplier, timespan, from_date, to_date):
+#     # Construct the URL for the Polygon API request
+#     url = f"{BASE_URL}/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={os.getenv('POLYGON_API_KEY')}"
+    
+#     # Make the request to the Polygon API
+#     response = requests.get(url)
+    
+#     # Check if the request was successful
+#     if response.status_code == 200:
+#         # Return the JSON response from Polygon API
+#         return jsonify(response.json()), 200
+#     else:
+#         # Return an error message if something went wrong with the request
+#         return jsonify({"error": "Failed to fetch data from Polygon API", "status_code": response.status_code}), response.status_code
+        
+if __name__ == '__main__':
+    app.run(debug=True)
